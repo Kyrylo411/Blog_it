942bcefe95c816c40b56fbec3b3ed9822982fb85
kyrylo <kirillomelcenko411@gmail.com>
2024-06-03 13:21:51 +0300

    added tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginUserName/getLoginUserName.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getLoginUserName } from './getLoginUserName';
+
+describe('getLoginLoading.test', () => {
+	test('should return correct username', () => {
+		const state: DeepPartial<StateSchema> = {
+			loginForm: {
+				username: 'admin'
+			}
+		}
+		expect(getLoginUserName(state as StateSchema)).toEqual('admin')
+	});
+	test('should work with empty state', () => {
+		const state: DeepPartial<StateSchema> = {}
+		expect(getLoginUserName(state as StateSchema)).toEqual('')
+	})
+})
